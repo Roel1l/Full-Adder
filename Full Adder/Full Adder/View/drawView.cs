7cfa8991fead33a694843c11ac11bd793ceb5877
Alec <alec@Alec-Laptop>
2016-06-25 15:20:43 +0200

    iets

--- Full Adder/Full Adder/View/drawView.cs
+++ Full Adder/Full Adder/View/drawView.cs
@@ -11,9 +11,36 @@ namespace Full_Adder
     {
         public void draw(Dictionary<string, INode>  _nodeDictionary)
         {
+            Dictionary<string, INode> probes = new Dictionary<string,INode>();
             foreach (var node in _nodeDictionary)
             {
                 node.Value.calculateOutput();
+
+                if(node.Value.ToString() != "Full_Adder.Nodes.PROBE"){
+
+                    Boolean mInput = false;
+
+                    Console.WriteLine("------------------------------------");
+                    Console.WriteLine(node.Key + "     " + node.Value.GetType());
+                    Console.Write("Input(s): ");
+                    foreach (int i in node.Value.input)
+                    {
+                        Console.Write(i);
+                        if (node.Value.input.Count > 1 && !mInput)
+                        {
+                            Console.Write(" + ");
+                            mInput = true;
+                        }
+                    }
+                    Console.WriteLine("");
+                    Console.WriteLine("Output: " + node.Value.output);
+                }
+                else
+                {
+                    probes.Add(node.Key, node.Value);
+                }
+            }
+            foreach(var node in probes){
                 Boolean mInput = false;
 
                 Console.WriteLine("------------------------------------");
@@ -31,8 +58,7 @@ namespace Full_Adder
                 Console.WriteLine("");
                 Console.WriteLine("Output: " + node.Value.output);
             }
-            Console.ReadLine();
-
+            Console.ReadKey();
         }
     }
 }
